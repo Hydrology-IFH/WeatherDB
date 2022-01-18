700e4cec98cc6a6eb7906ee7d2a750d390025124
Max Schmit <maxschm@hotmail.com>
2022-01-18 11:33:59 +0100

    change secret settings example

--- secretSettings_example.py
+++ secretSettings_example.py
@@ -1,7 +1,7 @@
 # secret settings for weatherDB module
 
-DB_HOST = "fuhys011.public.ads.uni-freiburg.de"
+DB_HOST = "" # fill in the host
 DB_PORT = 5432
 DB_WEA_NAME = "weather"
-DB_WEA_USER = "weather_user"
+DB_WEA_USER = "" # fill in the user
 DB_WEA_PWD = "" #fill in the password
