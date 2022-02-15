08fe3494dbb987119f3e3b19170bfd4388920dc0
Max Schmit <maxschm@hotmail.com>
2022-02-15 15:46:44 +0100

    error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sync.ffs_db']
 source_suffix = [".rst", ".md"]
 
 autodoc_default_options = {
-	'special-members': '__init__'
+	'special-members': '__init__',
     'member-order': 'bysource'
 }
 
