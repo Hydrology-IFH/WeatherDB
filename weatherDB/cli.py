dbe5b76912abb9e9dfe87b541234c1136ea3ec2f
maxschmi <maxschm@yahoo.de>
2023-01-30 16:29:18 +0100

    change cli to work from anywhere

--- weatherDB/cli.py
+++ weatherDB/cli.py
@@ -1,6 +1,8 @@
 import click
-import sys
-sys.path.insert(0, ".")
+import sys, os
+sys.path.insert(
+    0, 
+    os.path.split(os.path.abspath(os.path.split(__file__)[0]))[0])
 import weatherDB
 
 @click.command()
@@ -10,5 +12,6 @@ def update_db():
 
 # cli
 if __name__=="__main__":
+    click.echo("starting updating the database")
     update_db()
 
