1ada2ad960f0df836429e8262cc8a6809a13d614
maxschmi <maxschm@yahoo.de>
2023-06-15 11:28:22 +0200

    Dont show toctree in navbar of changelog

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,6 +86,10 @@ html_theme_options = {
     'navigation_depth': 9
 }
 
+html_css_files = [
+    'custom.css',
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
