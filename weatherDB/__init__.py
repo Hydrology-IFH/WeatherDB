96d9a888cc99e6987102aff2af10f1f71bc92028
maxschmi <maxschm@yahoo.de>
2023-08-28 16:30:36 +0200

    prepare new release

--- weatherDB/__init__.py
+++ weatherDB/__init__.py
@@ -3,7 +3,7 @@ import os
 __author__ = "Max Schmit"
 __email__ = "max.schmit@hydrology.uni-freiburg.de"
 __copyright__ = "Copyright 2023, Max Schmit"
-__version__ = "0.0.39"
+__version__ = "0.0.40"
 
 if not ("WEATHERDB_MODULE_INSTALLING" in os.environ \
         and os.environ["WEATHERDB_MODULE_INSTALLING"]=="True"):
